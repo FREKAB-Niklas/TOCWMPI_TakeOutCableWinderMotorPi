6e08210aebe657f52ad12cdadc639306f6855a16
FREKAB-Niklas <niklas@frekab.se>
2024-08-05 11:25:35 +0200

    Test

---
+++ stepper_control.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+# GPIO pin setup
+PUL = 17  # Pulse pin
+DIR = 27  # Direction pin
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(PUL, GPIO.OUT)
+GPIO.setup(DIR, GPIO.OUT)
+
+# Set direction
+GPIO.output(DIR, GPIO.HIGH)  # Change to GPIO.LOW for opposite direction
+
+try:
+    while True:
+        GPIO.output(PUL, GPIO.HIGH)
+        time.sleep(0.001)  # Adjust pulse width for motor speed
+        GPIO.output(PUL, GPIO.LOW)
+        time.sleep(0.001)
+except KeyboardInterrupt:
+    pass
+finally:
+    GPIO.cleanup()
